4b6b6a4c77c55c814435f98eb7f214d4d9cef6d5
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-03-07 09:45:13 +0100

    Created DTO for Users

---
+++ ResumeHandler/DTOs/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeHandler.DTOs;
+
+public class UserDto
+{
+    [Required]
+    public required string FirstName { get; set; }
+
+    [Required]
+    public required string LastName { get; set; }
+
+    [Required]
+    public required string Description { get; set; }
+}
