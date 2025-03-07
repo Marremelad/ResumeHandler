f99563de3919c67afa0c4a3e557d226881e2f8fc
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-03-07 09:42:45 +0100

    Created Response class to handle errors

---
+++ ResumeHandler/Common/Response.cs
@@ -0,0 +1,18 @@
+﻿namespace ResumeHandler.Common;
+
+public class Response<T>
+{
+    public bool IsSuccess { get; }
+    public T Value { get; }
+    public string? Error { get; }
+
+    private Response(bool isSuccess, T value, string? error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+
+    public static Response<T> Success(T value) => new Response<T>(true, value, null);
+    public static Response<T?> Failure(string error) => new Response<T?>(false, default, error);
+}
